86cc2c3bd83426512b0ed3867efe9e381a70fb10
donoghuc <cas.donoghue@gmail.com>
2022-02-04 12:11:47 -0800

    see what facts look like

--- modules/cygnus/plans/enforce_policy.pp
+++ modules/cygnus/plans/enforce_policy.pp
@@ -7,13 +7,15 @@ plan cygnus::enforce_policy(
     $target.set_var('cygnus_policies', $target_policies[$target.name]['policies'])
     $payload = {
       'certname' => $target.name,
+      # Unfortunately environmnet is a required key
+      'environment' => 'production',
       'producer' => 'policy',
       'producer_timestamp' => Timestamp.new.strftime('%FT%T%:z'),
       'values' => $target.facts(),
     }
     puppetdb_command('replace_facts', 5, $payload)
   }
-  
+
   $result = apply($targets) {
     include $cygnus_policies
   }
