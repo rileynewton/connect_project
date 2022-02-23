4b65ee60d4f7fb312914314bba4571b01cd04502
rileynewton <95654936+rileynewton@users.noreply.github.com>
2022-02-23 12:28:34 -0800

    Removed catch error plans

--- plans/catch_error_application.pp
+++
@@ -1,5 +0,0 @@
-plan connect_project::catch_error_application (TargetSpec $targets, Boolean $catch) {
-  return apply($targets, _catch_errors => $catch) {
-    package {'nonexistentpackagename': }
-  }
-}
